9c5c9f6aca44f2176fedb2bf8bdd675c56f171a6
linder3hs <linderhassinger00@gmail.com>
2023-05-26 16:09:00 -0500

    Initial Commit

---
+++ spotify-a/spotify_aApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_aApp.swift
+//  spotify-a
+//
+//  Created by Linder Anderson Hassinger Solano    on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct spotify_aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
